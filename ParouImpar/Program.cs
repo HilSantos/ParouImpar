547715660027ece7c545d1d6a1953ef3e4a2c5bb
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-09 17:23:55 -0300

    Adicionar arquivos de projeto.

---
+++ ParouImpar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParouImpar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int par, impar, val;
+            par = 0;
+            impar = 1;
+
+            Console.WriteLine("Digite o valor: ");
+            val = Convert.ToInt32(Console.ReadLine());
+
+            if (par <= 10)
+            {
+                Console.WriteLine("Par");
+            }
+            else 
+            {
+                Console.WriteLine("Impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
